c0cf322865d89a858c98bc18b76a0c024685139c
wpsadi <adityaislegend@outlook.com>
2024-02-07 18:39:05 +0530

    save

--- UTILITY/SaveImageOnGit.js
+++ UTILITY/SaveImageOnGit.js
@@ -7,42 +7,17 @@ import path from "path"
 // Path to your Git repository
 // console.log(path.relative())
 // const repoPath = "D:/junk/UploadTrash/";
-const repoPath1 = "E:/ACM-Backend/"
 const repoPath2 = "E:/ACM-Backend/upload"
-// console.log(repoPath)
 
-
-// Path to the image file you want to commit
-// const imagePath = 'path/to/your/image.jpg';
-
-// Read the image file
 
 export const saveImageOnGit = async(imagePath)=>{
     console.log(imagePath)
-    // fs.readFile(imagePath, (err, data) => {
-    //     if (err) {
-    //         console.error('Error reading image file:', err);
-    //         return;
-    //     }
     
-    //     // Initialize simple-git with the repository path
-    //     const git = simpleGit(repoPath1);
-    
-    //     // Add the image file to the staging area
-    //     git.add(imagePath)
-    //         .then(() => {
-    //             // Commit the changes with a message
-    //             return git.commit('Add image file: ' + imagePath);
-    //         })
-    //         .then(() => {
-    //             git.push('origin', 'main')
-    //             console.log('Image file committed successfully.');
-    //         })
-    //         .catch((err) => {
-    //             console.error('Error committing image file:', err);
-    //         });
-    // });
+    /*The path coming from multer req.file.path contains the "/upload" but we don't need it for because the git is initialised for that rep only*/
     fs.readFile(imagePath, (err, data) => {
+        imagePath = path.resolve(imagePath)
+        console.log(imagePath)
+        // imagePath = path.join(imagePath.slice(1,imagePath.length))
         if (err) {
             console.error('Error reading image file:', err);
             return;
