78f441fa4aeac35c6d7b3b817ee566635a0fd854
wpsadi <adityaislegend@outlook.com>
2024-02-16 14:42:20 +0530

    GitHub link now use GitLFS

--- UTILITY/SaveImageOnGit.js
+++ UTILITY/SaveImageOnGit.js
@@ -4,6 +4,7 @@ import { Octokit } from "@octokit/rest";
 import fs from "fs";
 import "../environment.js"
 import path from "path"
+import crypto from "crypto"
 
 // Authentication
 const octokit = new Octokit({
@@ -16,11 +17,32 @@ const repo = process.env.Github_Storage_rep_name;
 const branch = "main";
 
 // Path to the image file
+function calculateFileHash(content) {
+  const hash = crypto.createHash("sha256");
+  hash.update(content);
+  return hash.digest("hex");
+}
 
 async function uploadToGitHub(imagePath,uploadPath) {
   // console.log(path.resolve(imagePath))
   try {
     const imageContent = fs.readFileSync(path.resolve(imagePath));
+
+    const hash = calculateFileHash(imageContent);
+    const size = imageContent.length;
+    const pointerContent = `version https://git-lfs.github.com/spec/v1\noid sha256:${hash}\nsize ${size}`;
+    
+    const responsePointer = await octokit.repos.createOrUpdateFileContents({
+      owner,
+      repo,
+      path: `${uploadPath}.gitattributes`, // Append .png to the upload path to ensure it's treated as an LFS object
+      message: "Created LFS",
+      content: Buffer.from(pointerContent).toString('base64'),
+      branch
+    });
+
+
+
     const response = await octokit.repos.createOrUpdateFileContents({
       owner,
       repo,
@@ -42,21 +64,53 @@ async function deleteFromGitHub(filePath) {
   try {
     // const filePath = ; // Path to the file to delete
 
-    const { data: { sha } } = await octokit.repos.getContent({
-      owner,
-      repo,
-      path: filePath,
-    });
+    //remove the pointer
+    try{
+      let shaPointer = await octokit.repos.getContent({
+        owner,
+        repo,
+        path: `${filePath}.gitattributes`,
+      });
+  
+      shaPointer = shaPointer.data.sha
+  
+      await octokit.repos.deleteFile({
+        owner,
+        repo,
+        path: `${filePath}.gitattributes`,
+        message: "Delete attribute file via API",
+        sha: shaPointer,
+      });
+    }catch(e){
+      null
+    }
 
-    await octokit.repos.deleteFile({
-      owner,
-      repo,
-      path: filePath,
-      message: "Delete file via API",
-      sha: sha,
-    });
+    //remove the file
+    try{
+      let shaFile = await octokit.repos.getContent({
+        owner,
+        repo,
+        path: filePath,
+      });
+  
+      shaFile = shaFile.data.sha
+  
+      await octokit.repos.deleteFile({
+        owner,
+        repo,
+        path: filePath,
+        message: "Delete file via API",
+        sha: shaFile,
+      });
+    }catch(e){
+      null
+    }
+
+     
+
     return true
   } catch (error) {
+    // console.log(error.message)
     return false
   }
 }
