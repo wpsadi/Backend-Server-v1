d2bd315e28166edbed5778d078aa799da2c9c679
wpsadi <adityaislegend@outlook.com>
2024-02-19 19:17:15 +0530

    Implemented a more strict API call limit

--- API-calling-rate-limit/limit-perr-sec-calls.js
+++ API-calling-rate-limit/limit-perr-sec-calls.js
@@ -1,9 +1,16 @@
 import AppError from "../UTILITY/errClass.js";
 
 let MassRequestIP = new Object();
-
+let Hr3Ban = new Array()
+let indexHr3Ban = 0
 export const limit_API_calls = async(req,res,next)=>{
     const ip = req.clientIp
+
+    if (Hr3Ban.includes(ip) && (ip != "::1")){
+        return next(new AppError("This IP has a banned for any response for 1hr due excessive API calls ",429));
+    }
+
+
     if (Object.keys(MassRequestIP).includes(ip)){
         MassRequestIP[ip]++;
     }
@@ -16,6 +23,13 @@ export const limit_API_calls = async(req,res,next)=>{
     }
 
     if ((MassRequestIP[`${ip}`]>10) && (ip != "::1")){
+        if (MassRequestIP[`${ip}`]>18){
+            Hr3Ban[indexHr3Ban++] = `${ip}`
+            setTimeout(()=>{
+                delete Hr3Ban[indexHr3Ban-1]
+            },1000*60*60)
+        }
+        
         return next(new AppError("Restricted By API Rate limit -[ 10 Request/sec]",429));
     }
 
